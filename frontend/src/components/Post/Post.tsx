d668c0cc5f8acc806a19e6bbeaf34c8a9e331e99
BradSavary <brad.savary@etu.unilim.fr>
2025-03-20 15:05:25 +0100

    Enhance Post component layout with avatar; update timeAgo utility for concise time representation; improve Feed component styling with Threads icon

--- frontend/src/components/Post/Post.tsx
+++ frontend/src/components/Post/Post.tsx
@@ -8,13 +8,16 @@ interface PostProps {
 
 function Post({ username, content, date }: PostProps): JSX.Element {
   return (
-    <div className=" p-4 border-b border-custom-light-gray w-full" >
-      <div className=" flex items-center justify-between mb-2">
-        <span className=" font-bold mr-2 text-custom">{username}</span>
-        <span className=" text-custom-light-gray text-sm ">{date}</span>
+    <div className='flex flex-row w-full'>
+      <img src="https://avatar.iran.liara.run/public" className='rounded-full max-w-8 max-h-8 mt-4 ml-2' alt="Post image" />
+      <div className="p-4 border-b border-custom-light-gray w-full">
+      <div className="flex items-center justify-between mb-2">
+        <span className="font-bold mr-2 text-custom">{username}</span>
+        <span className="text-custom-light-gray text-sm">{date}</span>
       </div>
-      <div className=" text-gray-800 text-custom-light-gray max-w-full">
+      <div className="text-gray-800 text-custom-light-gray max-w-full">
         {content}
+      </div>
       </div>
     </div>
   );
--- frontend/src/routes/feed.tsx
+++ frontend/src/routes/feed.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState, JSX } from 'react';
 import { fetchFeedPosts } from '../data/post';
 import { Post } from '../components/Post/Post';
 import { timeAgo } from '../lib/utils';
+import { ThreadsIcon } from '../ui/LogoIcon/threads';
 
 interface PostData {
   id: number;
@@ -46,7 +47,8 @@ function Feed(): JSX.Element {
   }
 
   return (
-    <section className='bg-custom pb-15 '>
+    <section className='bg-custom pb-15 flex flex-col items-center w-full pt-6'>
+      <ThreadsIcon size="large" className='self-center' />
       {posts.map(post => (
         <Post
           key={post.id}
